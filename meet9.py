d5aaaa2c35f1a1dfe64665ca7b711f5b2693cffb
zumajji <147676505+zumajji@users.noreply.github.com>
2024-06-13 14:57:01 +0800

    Add files via upload

---
+++ meet9.py
@@ -0,0 +1,12 @@
+#Belajar mulit percabangan
+
+nama = input ("masukan nama :")
+if nama=="edy": # kondisi 1
+    print("dosen ganteng") #aksi true 1
+elif nama=="dila": #kondisi 2
+    print("mahasiswi cantik") #aksi true 2
+elif nama=="putra": #kondisi 3
+    print("mahasiswa gk ganteng") #aksi true 3
+else:
+    print("anda bukan siapa-siapa") #false
+    print("program selesai") #aksi end
