8531b3fcf91922c02a244580aeafad9453d1a501
zumajji <147676505+zumajji@users.noreply.github.com>
2024-06-20 14:24:33 +0800

    Add files via upload

---
+++ meet10.py
@@ -0,0 +1,40 @@
+# lartihan perulangan
+
+#bil = 1 
+#print(bil)
+#bil = bil + 2
+#print(bil)
+#bil = bil + 2
+#print(bil)
+#bil = bil + 2
+#print(bil)
+#bil = bil + 2
+#print(bil)
+#bil = bil + 2
+#print(bil)
+
+#if kondisi:
+ #   aksi
+ 
+bil = [0,1,2,3,4] # ini menggunakan list
+print(bil)
+    
+for i in bil :
+    print (f"nilai i sekarang: {i}")
+    print("selesai 1")
+    
+bil2 = range(5) # ini menggunakan range
+for i in bil2 :
+    print (f"nilai i sekarang: {i}")
+print("selesai 2")
+    
+bil3 = range(1,5) # ini menggunakan range
+for i in bil3 :
+    print("pak edy ganteng")
+    #print (f"nilai i sekarang: {i}")
+print("selesai 3")
+    
+         
+    
+    
+        
